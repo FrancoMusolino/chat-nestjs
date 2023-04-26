ad77516a6a3f83909cdbdc330918b5ff470c4428
FrancoMusolino <francomusolino55@gmail.com>
2023-04-26 19:55:21 -0300

    (deploy) fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Bienvenidos al chat nestjs App';
+    return 'Bienvenidos al chat nestjs App!!';
   }
 }
